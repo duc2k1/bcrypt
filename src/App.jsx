cab7135286ca08d82efdc91516cd9e8f5d71ff41
duc622 <duc.phan622@gmail.com>
2021-09-28 19:41:39 +0700

    disable inspect element and view page source option on site

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
+  document.onkeydown = (e) => {
+    if (e.keyCode === 123 || e.ctrlKey) return false;
+  };
   return (
     <div className="container">
       <Header />
