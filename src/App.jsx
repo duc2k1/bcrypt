be99d1bc85ee1ec48085b84bd349588ab086eade
duc622 <duc.phan622@gmail.com>
2021-09-28 19:46:42 +0700

    update prev commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ import Main from "./components/Main";
 
 export default function App() {
   document.onkeydown = (e) => {
-    if (e.keyCode === 123 || e.ctrlKey) return false;
+    if (e.keyCode === 123) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode === "S".charCodeAt(0)) {
+      return false;
+    }
   };
   return (
     <div className="container">
